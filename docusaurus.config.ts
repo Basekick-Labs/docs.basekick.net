1633e6dc4ee260c2013f72b53a7c5f6f6201e766
Ignacio Van Droogenbroeck <ignacio@vandroogenbroeck.net>
2025-12-02 12:55:06 -0300

    chore: Update OpenGraph image to Arc logo
    Replace default Docusaurus dinosaur with Arc branding.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -53,8 +53,8 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    // OpenGraph social card
+    image: 'img/arc.png',
     colorMode: {
       respectPrefersColorScheme: true,
     },
